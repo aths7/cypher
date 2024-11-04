9fb7a0dfc09c4a1846f3ff84cb648548df6ece66
aths7 <atharvaw007@gmail.com>
2024-11-04 13:07:55 +0530

    adding files to master

--- main.ts
+++ main.ts
@@ -1,85 +1,57 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CypherPluginSettings {
+	cypherStyle: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: CypherPluginSettings = {
+	cypherStyle: 'diagrammatic'
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CypherPlugin extends Plugin {
+	settings: CypherPluginSettings;
+	private typingTimeout: number | undefined;
+	private isTypingHandled: boolean = false;
+	private isCypherEnabled: boolean = false;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		// Ribbon icon for toggling cypher mode
+		this.addRibbonIcon('dice', 'Cypher Text', () => {
+			this.isCypherEnabled = !this.isCypherEnabled;
+			if (this.isCypherEnabled) {
+				new Notice('Cypher Plugin Activated!');
+				this.applyCypher();
+			} else {
+				new Notice('Cypher Plugin Deactivated!');
+				this.revertCypher();
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		}).addClass('cypher-plugin-ribbon-class');
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		// Command to cypher selected text
+		this.addCommand({
+			id: 'cypher-transform',
+			name: 'Transform Selected Text to Cypher',
+			editorCallback: (editor: Editor, view: MarkdownView) => this.transformText(editor),
+		});
+
+		// Add settings tab
+		this.addSettingTab(new CypherSettingTab(this.app, this));
+
+		// Editor change event for real-time typing transformation
+		this.registerEvent(
+			this.app.workspace.on("editor-change", () => {
+				const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+				if (editor && this.isCypherEnabled) {
+					this.debounceTyping(editor, 500);
 				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			})
+		);
 	}
 
 	onunload() {
-
+		console.log("Cypher Plugin unloaded.");
 	}
 
 	async loadSettings() {
@@ -89,45 +61,131 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	// Debounce function to delay the execution of handleTyping
+	debounceTyping(editor: Editor, delay = 500) {
+		if (this.isTypingHandled) return;
+
+		// Clear existing timeout, if any
+		if (this.typingTimeout) clearTimeout(this.typingTimeout);
+
+		this.isTypingHandled = true;
+		this.typingTimeout = window.setTimeout(() => {
+			this.handleTyping(editor);
+			this.isTypingHandled = false;
+		}, delay);
+	}
+
+	// Cypher transformation with distinct symbols for uppercase and lowercase
+	toCypherText(input: string): string {
+		const iconCypherMap: Record<string, string> = {
+			A: "🔺", B: "🅱️", C: "🌜", D: "🔷", E: "🍏",
+			F: "🍀", G: "🌀", H: "🏠", I: "🦋", J: "🎷",
+			K: "🔑", L: "🌿", M: "🏆", N: "🎵", O: "💎",
+			P: "🎩", Q: "🎈", R: "🌹", S: "🌞", T: "🎾",
+			U: "🐢", V: "🎻", W: "🍉", X: "❌", Y: "🌱",
+			Z: "⚡️", 
+			a: "🍎", b: "🍌", c: "🍒", d: "🍩", e: "🌽",
+			f: "🐸", g: "🦍", h: "🍯", i: "🍦", j: "🌶️",
+			k: "🥝", l: "🦁", m: "🍈", n: "🥜", o: "🍊",
+			p: "🍕", q: "👑", r: "🐀", s: "🐍", t: "🌮",
+			u: "🐡", v: "🍇", w: "💫", x: "🍪", y: "🦓",
+			z: "🍫"
+		};
+		
+	
+		let result = "";
+		let wasLastCharSpace = false;
+	
+		for (let i = 0; i < input.length; i++) {
+			const char = input[i];
+	
+			if (char === " ") {
+				if (!wasLastCharSpace) {
+					result += "  ";
+					wasLastCharSpace = true;
+				}
+			} else {
+				result += iconCypherMap[char] || char;
+				wasLastCharSpace = false;
+			}
+		}
+	
+		return result;
+	}
+
+	// Revert cyphered text back to normal
+	revertCypherText(input: string): string {
+		const reverseCypherMap: Record<string, string> = {
+			"🔺": "A", "🅱️": "B", "🌜": "C", "🔷": "D", "🍏": "E",
+			"🍀": "F", "🌀": "G", "🏠": "H", "🦋": "I", "🎷": "J",
+			"🔑": "K", "🌿": "L", "🏆": "M", "🎵": "N", "💎": "O",
+			"🎩": "P", "🎈": "Q", "🌹": "R", "🌞": "S", "🎾": "T",
+			"🐢": "U", "🎻": "V", "🍉": "W", "❌": "X", "🌱": "Y",
+			"⚡️": "Z", 
+			"🍎": "a", "🍌": "b", "🍒": "c", "🍩": "d", "🌽": "e",
+			"🐸": "f", "🦍": "g", "🍯": "h", "🍦": "i", "🌶️": "j",
+			"🥝": "k", "🦁": "l", "🍈": "m", "🥜": "n", "🍊": "o",
+			"🍕": "p", "👑": "q", "🐀": "r", "🐍": "s", "🌮": "t",
+			"🐡": "u", "🍇": "v", "💫": "w", "🍪": "x", "🦓": "y",
+			"🍫": "z"
+		};
+		
+
+		return Array.from(input).map(char => reverseCypherMap[char] || char).join("");
+	}
+
+	handleTyping(editor: Editor) {
+		const cursor = editor.getCursor();
+		const lineText = editor.getLine(cursor.line);
+		const modifiedText = this.isCypherEnabled ? this.toCypherText(lineText) : this.revertCypherText(lineText);
+		editor.setLine(cursor.line, modifiedText);
+	}
+
+	applyCypher() {
+		const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+		if (editor) {
+			const fullText = editor.getValue();
+			editor.setValue(this.toCypherText(fullText));
+		}
+	}
+
+	revertCypher() {
+		const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+		if (editor) {
+			const fullText = editor.getValue();
+			editor.setValue(this.revertCypherText(fullText));
+		}
+	}
+
+	transformText(editor: Editor) {
+		const selections = editor.getSelection();
+		const cypheredText = this.isCypherEnabled ? this.toCypherText(selections) : this.revertCypherText(selections);
+		editor.replaceSelection(cypheredText);
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class CypherSettingTab extends PluginSettingTab {
+	plugin: CypherPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CypherPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
+		containerEl.createEl('h2', { text: 'Cypher Plugin Settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Cypher Style')
+			.setDesc('Choose the style for cyphering text')
+			.addDropdown(dropdown => dropdown
+				.addOption('diagrammatic', 'Diagrammatic')
+				.setValue(this.plugin.settings.cypherStyle)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.cypherStyle = value;
 					await this.plugin.saveSettings();
 				}));
 	}
